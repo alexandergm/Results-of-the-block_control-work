48e2edfc546a49617da90e14f472dd1135086e03
Alexander Vasilyev <sasha_261@outlook.com>
2022-09-29 21:08:15 +0300

    Complete Task

---
+++ Programm/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arr1 = new string[5] {"HelloWorld", "04011994", "hel", "mo", "res"};
+string[] arr2 = new string[arr1.Length];
+CheckArray(arr1, arr2);
+PrintArray(arr2);
+
+void CheckArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if(arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
